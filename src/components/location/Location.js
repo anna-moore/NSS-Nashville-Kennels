e13c58de00628c352a6fd5cb15234a05f689d890
anna-moore <ammoore434@gmail.com>
2021-01-21 10:54:11 -0600

    refactor with navbar

---
+++ src/components/location/Location.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Location.css"
+
+export const LocationCard = () => (
+    <section className="location">
+        <h3 className="location__name">a location name</h3>
+        <div className="location__address">an address</div>
+    </section>
+)
--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
 
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import { Kennel } from "./components/Kennel"
+// import './index.css'
 
 // ReactDOM.render(
 //   <React.StrictMode>
-//     <App />
+//     <Kennel />
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
 
 import React from "react"
 import ReactDOM from "react-dom"
-import { Kennel } from "./components/Kennel"
-import './index.css'
+import { BrowserRouter as Router } from "react-router-dom"
+import { Kennel } from "./components/Kennel.js"
+import "./index.css"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Kennel />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Router>
+            <Kennel />
+        </Router>
+    </React.StrictMode>,
+    document.getElementById("root")
+)
