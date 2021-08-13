7c4e3bc98d527ec6b05efe4345f913004c86ce5b
anna-moore <ammoore434@gmail.com>
2021-08-12 20:53:16 -0500

    update nav bar; update home page to include user name

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,33 @@
 import React from "react";
-import { PropsAndState } from './PropsAndState'
+import { PropsAndState } from "./PropsAndState";
+import { CustomerContext } from "./customer/CustomerProvider";
 
-export const Home = () => (
+export const Home = () => {
+  const { customers, getCustomers } = useContext(CustomerContext);
+  let userName;
+
+  useEffect(() => {
+    getCustomers();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const userId = localStorage.getItem("kennel_customer");
+  const user = customers.find((c) => c.id === parseInt(userId));
+  if (!user) {
+    userName = "user";
+  } else {
+    userName = user.name;
+  }
+
+  return (
     <>
-        <h2>Nashville Kennels</h2>
-        <small>Loving care when you're not there.</small>
+      <h2>Nashville Kennels</h2>
+      <small>Loving care when you're not there.</small>
 
-        <address>
-            <div>Visit Us at the Nashville North Location</div>
-            <div>500 Puppy Way</div>
-        </address>
-        <PropsAndState yourName={"Isabel"} />
-        
+      <address>
+        <div>Visit Us at the Nashville North Location</div>
+        <div>500 Puppy Way</div>
+      </address>
+      <PropsAndState yourName={"Isabel"} />
     </>
-)
+  );
+};
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,25 +1,42 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import "./NavBar.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 export const NavBar = (props) => {
-    return (
-        <ul className="navbar">
-            <li className="navbar__item active">
-                <Link className="navbar__link" to="/">NSS Kennels</Link>
-            </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/locations">Locations</Link>
-            </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/animals">Animals</Link>
-            </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/customers">Customers</Link>
-            </li>
-            <li className="navbar__item">
-                <Link className="navbar__link" to="/employees">Employees</Link>
-            </li>
-        </ul>
-    )
-}
+  return (
+    <ul className="navbar">
+      <li className="navbar__item active">
+        <Link className="navbar__link" to="/">
+          NSS Kennels
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link className="navbar__link" to="/locations">
+          Locations
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link className="navbar__link" to="/animals">
+          Animals
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link className="navbar__link" to="/customers">
+          Customers
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link className="navbar__link" to="/employees">
+          Employees
+        </Link>
+      </li>
+      <li className="navbar__item">
+        <Link className="navbar__link" to="/">
+          <button className="logout-button" onClick={logout}>
+            Log Out
+          </button>
+        </Link>
+      </li>
+    </ul>
+  );
+};
